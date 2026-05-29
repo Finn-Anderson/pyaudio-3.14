821daba6595d7488529c1cde35f1161dd1efaa9a
Finn Anderson <88798320+Finn-Anderson@users.noreply.github.com>
2026-05-29 23:44:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,8 +45,7 @@ elif sys.platform == "darwin":
     subprocess.run(["brew", "install", "portaudio"])
 elif sys.platform == "win32":
     subprocess.run(["vcpkg", "install", "portaudio:x64-windows-static"])
-    print(os.environ["VCPKG_PATH"])
-    print(os.environ["VCPKG_INSTALLATION_ROOT"] + "\installed\x64-windows-static")
+    print(os.environ)
     os.environ["VCPKG_PATH"] = os.environ["VCPKG_INSTALLATION_ROOT"] + "\installed\x64-windows-static"
 
 def setup_extension():
