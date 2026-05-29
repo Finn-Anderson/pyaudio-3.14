1de5888656c7218e6a5b1a8a58d31e687e77bd46
Finn Anderson <88798320+Finn-Anderson@users.noreply.github.com>
2026-05-29 23:02:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ elif sys.platform == "darwin":
     subprocess.run(["brew", "install", "portaudio"])
 elif sys.platform == "win32":
     subprocess.run(["vcpkg", "install", "portaudio:x64-windows-static"])
+    os.putenv('COMSPEC',r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe')
     subprocess.run(["$env:VCPKG_PATH", "=", "'$env:VCPKG_INSTALLATION_ROOT\installed\x64-windows-static'"], shell=True)
 
 def setup_extension():
